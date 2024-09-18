2b09e8fae4288f66e9bedfe7d0901ee12331d470
ATOROYO <davidatoroyosika@gmail.com>
2024-09-18 20:20:18 +0300

    Created function to handle questions and ansers toggle

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -122,3 +122,16 @@ function clearErrors() {
     input.classList.remove("invalid");
   });
 }
+
+// Function handling toggling of Faq question and answers .
+document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        const answer = item.querySelector('.faq-answer');
+        const toggleIcon = item.querySelector('.toggle-icon');
+
+        question.addEventListener('click', () => {
+            const isVisible = answer.style.display === 'block';
+})
