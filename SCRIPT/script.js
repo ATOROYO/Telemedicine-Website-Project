7b522d2e0b2678a806105dbab5f81f22168d965a
ATOROYO <davidatoroyosika@gmail.com>
2024-09-18 18:15:08 +0300

    Did a minor change to index.html

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,4 +1,4 @@
-// Script fot the toggle button
+// Script fot the toggle button / Toggle functionality
 function toggleMenu() {
   const nav = document.querySelector(".mobile-nav");
   nav.style.display = nav.style.display === "block" ? "none" : "block";
