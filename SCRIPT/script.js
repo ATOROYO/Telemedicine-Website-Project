10a613c1641b0eb8df438c87f1306e3526e9c1d2
ATOROYO <davidatoroyosika@gmail.com>
2024-09-18 18:27:06 +0300

    Created variable for the ids

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -3,3 +3,16 @@ function toggleMenu() {
   const nav = document.querySelector(".mobile-nav");
   nav.style.display = nav.style.display === "block" ? "none" : "block";
 }
+
+// Function for Form Validation for Consultation.html
+function validateForm() {
+  // Assigning variable to ids
+  let isValid = true;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const specialty = document.getElementById("specialty").value;
+  const doctor = document.getElementById("doctor").value;
+  const date = document.getElementById("appointment-date").value;
+  const time = document.getElementById("appointment-time").value;
+}
