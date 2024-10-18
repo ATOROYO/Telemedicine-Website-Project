81224850784b8f287f8230702193419294fd2053
ATOROYO <davidatoroyosika@gmail.com>
2024-10-18 21:21:45 +0300

    Made changes to test.js

--- SCRIPT/test.js
+++ SCRIPT/test.js
@@ -1,7 +1,1 @@
-// togle menu/nav-bar Script
-$(document).ready(function () {
-  $(".menu-btn").click(function () {
-    $(".nav-bar .menu").toggleClass("active");
-    $(".nav-bar i").toggleClass("active");
-  });
-});
+
